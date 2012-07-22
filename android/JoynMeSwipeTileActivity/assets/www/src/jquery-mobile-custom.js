d42f9954c76ee2884b593c0be9b9765abe6e855e
Arjun Baokar <arjunbaokar@gmail.com>
2012-07-21 20:37:22 -0700

    messed with the shifting of tiles (move them individually now), still need to do more

--- android/JoynMeSwipeTileActivity/assets/www/src/jquery-mobile-custom.js
+++ android/JoynMeSwipeTileActivity/assets/www/src/jquery-mobile-custom.js
@@ -8,9 +8,56 @@ $(function(){
 			// remove any existing swipe areas
 			$('.divSwipe').remove();
 			// add swipe event to the list item, removing it first (if it exists)
-			$('ul li').unbind('swiperight').bind('swiperight', function(e){
+			$('ul li').unbind('swiperight').bind('swiperight click', function(e){ // FIXME remove click for the phone
 				// reference the just swiped list item
 				var $li = $(this);
+				var tileId = e.target.id;
+
+				// remove all swipe divs first
+				$('.divSwipe').remove();
+				// create buttons and div container
+				var $divSwipe = $('<div class="divSwipe"></div>');
+				var $myBtn01 = $('<a>Button One</a>')
+								.attr({
+									'class': 'aSwipeBtn ui-btn-up-b',
+									'href': 'page.html'
+								});
+				var $myBtn02 = $('<a>Button Two</a>')
+								.attr({
+									'class': 'aSwipeBtn ui-btn-up-e',
+									'href': 'page.html'
+								});
+				// insert swipe div into list item
+				$li.prepend($divSwipe);
+
+				var name = '#t0';
+
+				$(name).animate({
+				    marginLeft: "3in",
+			  	}, 400 );
+
+				// insert buttons into divSwipe
+
+				//$divSwipe.prepend($myBtn01,$myBtn02).show(1000);
+
+				// add escape route for swipe menu
+				$('body').bind('tap', function(e){
+					// if the triggering object is a button, fire it's tap event
+					if (e.target.className.indexOf('aSwipeBtn') >= 0) $(e.target).trigger('click'); 
+					// remove any existing cancel buttons
+					$('.divSwipe').remove();
+					// remove the event
+					$('body').unbind('tap');
+				});
+			});
+			
+			// copied above and editing it now lol
+			
+			$('ul li').unbind('swipeleft').bind('swipeleft', function(e){
+				// reference the just swiped list item
+				var $li = $(this);
+				var tileId = e.target.id;
+				
 				// remove all swipe divs first
 				$('.divSwipe').remove();
 				// create buttons and div container
@@ -31,7 +78,7 @@ $(function(){
 				var name = '#r1';
 
 				$(name).animate({
-				    marginLeft: "3in",
+				    marginLeft: "-3in",
 			  	}, 400 );
 
 				// insert buttons into divSwipe
@@ -50,3 +97,5 @@ $(function(){
 			});
 
 })
+
+// to get what was clicked on, look at function(event) -> event is important because that what was clicked on. not $li. event.target.id (get the id of the targeted thing)
